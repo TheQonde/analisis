2836c5b0d1f15782da20619d7b19b2905597ccae
TheQonde <3117446070jose@gmail.com>
2023-05-24 09:40:29 -0500

    apdate1.1.1

--- bayes.py
+++ bayes.py
@@ -1,33 +1,46 @@
-# Definir las probabilidades condicionales de las porpabilidades de covid
-p_fatiga = 0.75
-p_tos_seca =  0.80
-p_dificultad_respirar = 0.90
-p_dolor_garganta = 0.65
-p_dolor_cabeza = 0.20
-p_dolor_cuerpo = 0.30
-p_escalofrios = 0.40
-p_secresion_nasal = 0.50
-p_perdida_sentido = 0.70
-p_fiebre = 0.80
-p_dolor_pecho = 0.70
+# Definir las probabilidades condicionales de las probabilidades de COVID-19
+probabilidades = {
+    'Fatiga': 0.75,
+    'Tos seca': 0.80,
+    'Dificultad para respirar': 0.90,
+    'Dolor de garganta': 0.65,
+    'Dolor de cabeza': 0.20,
+    'Dolor en el cuerpo': 0.30,
+    'Escalofríos': 0.40,
+    'Secresión nasal': 0.50,
+    'Pérdida del sentido del olfato': 0.70,
+    'Fiebre': 0.80,
+    'Dolor de pecho': 0.70
+}
 
-# Definir la prevalencia dle covid en la población
+# Definir la prevalencia del COVID-19 en la población
 p_infectado = 0.05
 
-#Definir los sintomas obsrevados en el paciente
-sintomas = ['Fatiga', 'Tos seca', 'Dificuktad para respirar', 'Dolor de garganta','Dolor de cabeza', 
-            'Dolor en el cuerpo', 'Escalofrios', 'Secresión nasal','Pérdida sentido del olfato', 'Fiebre',
-              'Dolor de pecho']
+# Definir los síntomas observados en el paciente
+sintomas = ['Fatiga', 'Tos seca', 'Dificultad para respirar', 'Dolor de garganta', 'Dolor de cabeza',
+            'Dolor en el cuerpo', 'Escalofríos', 'Secresión nasal', 'Pérdida del sentido del olfato', 'Fiebre',
+            'Dolor de pecho']
 
-# Calcular la probabilidad de que el paciente tenga covid 19
-p_sintomas_infectado = p_fatiga * p_tos_seca * p_dificultad_respirar * p_dolor_garganta * p_dolor_cabeza * p_dolor_cuerpo * \
-p_escalofrios * p_secresion_nasal * p_perdida_sentido * p_fiebre * p_dolor_pecho
 
-# Se asume que la probabilidad de tener covid 19 sin tener los sintomas es muy baja
-p_sintomas_no_infectado = 0.05 ** 11
+def calcular_probabilidad_covid(sintomas):
+    p_sintomas_infectado = 1.0
+    for sintoma in sintomas:
+        if sintoma in probabilidades:
+            p_sintomas_infectado *= probabilidades[sintoma]
+        else:
+            raise ValueError(f"Síntoma no válido: {sintoma}")
 
-p_sintomas = p_sintomas_infectado * p_infectado + p_sintomas_no_infectado * (1-p_infectado)
+    p_sintomas_no_infectado = 0.05 ** len(sintomas)
 
-p_infectado_sintomas = p_sintomas_infectado * p_infectado / p_sintomas
+    p_sintomas = p_sintomas_infectado * p_infectado + p_sintomas_no_infectado * (1 - p_infectado)
 
-print(f"La probabilidad que el paciente tenga covid dado los sintomas es de {p_infectado_sintomas}")
+    p_infectado_sintomas = p_sintomas_infectado * p_infectado / p_sintomas
+
+    return p_infectado_sintomas
+
+
+try:
+    p_infectado_sintomas = calcular_probabilidad_covid(sintomas)
+    print("La probabilidad de que el paciente tenga COVID-19 dado los síntomas es de {:.2f}".format(p_infectado_sintomas))
+except ValueError as e:
+    print(str(e))
